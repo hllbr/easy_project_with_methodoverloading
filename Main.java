a71e6ecdba9e5d0976ebe0b74d734182784bfe4f
Halil İbrahim KOÇAK <halibrahim.kocak@gmail.com>
2020-11-26 17:54:34 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void skorhesapla(String isim,int puan){
+        System.out.println(isim +" adlı oyuncunun "+puan +" puanı vardır.");
+    }
+    public static void skorhesapla(String isim){
+        System.out.println(isim+" adı belirtilen oyucunun puanı belirtilmemiştir...");
+    }
+    public static void skorhesapla(int puan){
+        System.out.println("ismi belirtilmemiş oyuncunun puanı" +puan);
+    }
+    public static void main(String[] args) {
+        skorhesapla("@prince");
+        skorhesapla(85);
+        skorhesapla("hDoublelbr", 58);
+        
+    }
+    
+}
